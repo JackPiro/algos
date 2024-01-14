d560be57f91823cf45125a65bdae85373f0afdc8
Jack Piro <jackpiro1133@gmail.com>
2024-01-13 16:30:19 -0800

    update

--- dojo/fundamentals_1.js
+++ dojo/fundamentals_1.js
@@ -1,16 +1,21 @@
 // Setting and Swapping
 // Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
 
-let myName = "Jack";
-let myNumber = 42;
+
+
+
 
 const setSwap = (var1, var2) => {
+    console.log("runing setSwap")
+    console.log(var1, var2);
     let holder = ""
-    var1 = holder;
-    var2 = var1;
     holder = var1;
+    var1 = var2;
+    var2 = holder;
     console.log(var1, var2);
 }
 
-setSwap()
+
+setSwap("Jack", 42);
+
 
