92d8a534224aa8ba6856a5826f9a7a4b7a401929
Jack Piro <jackpiro1133@gmail.com>
2024-01-13 16:44:50 -0800

    update

--- dojo/fundamentals_1.js
+++ dojo/fundamentals_1.js
@@ -1,12 +1,10 @@
+
 // Setting and Swapping
 // Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
 
 
-
-
-
 const setSwap = (var1, var2) => {
-    console.log("runing setSwap")
+    console.log("running setSwap")
     console.log(var1, var2);
     let holder = ""
     holder = var1;
@@ -15,7 +13,28 @@ const setSwap = (var1, var2) => {
     console.log(var1, var2);
 }
 
+// setSwap("Jack", 42);
 
-setSwap("Jack", 42);
+//Print -52 to 1066
+//Print integers from -52 to 1066 using a FOR loop.
 
+const printRange = (start, stop) => {
+    if(start < stop) {
+        for (let i = start; i <= stop; i++) {
+            console.log(i);
+        }
+    } else {
+        for (let i = start; i >= stop; i--) {
+            console.log(i)
+        }
+    }
+}
 
+// printRange(-56, 1066);
+
+//Don’t Worry, Be Happy
+//Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.
+
+const beCheerful = () => {
+
+}
