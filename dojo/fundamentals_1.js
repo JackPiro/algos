5902d4a1ac2ed87f9d9588ac5b302044c8ff73cb
Jack Piro <jackpiro1133@gmail.com>
2024-01-16 09:07:36 -0800

    update

--- dojo/fundamentals_1.js
+++ dojo/fundamentals_1.js
@@ -97,3 +97,12 @@ const birthDay = (dayOrMonth, monthOrDay) => {
 
 birthDay(17, 6)
 
+
+//Leap Year
+//Write a function that determines whether a given year is a leap year. If a year is divisible by four, it is a leap year, 
+//unless it is divisible by 100. However, if it is divisible by 400, then it is.
+
+const leapYear = () => {
+    
+}
+
