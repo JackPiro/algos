3706ca2586ee63aee0e0116162aa04514e809d96
Jack Piro <jackpiro1133@gmail.com>
2024-01-16 20:55:20 -0800

    update

--- dojo/fundamentals_1.js
+++ dojo/fundamentals_1.js
@@ -102,7 +102,13 @@ birthDay(17, 6)
 //Write a function that determines whether a given year is a leap year. If a year is divisible by four, it is a leap year, 
 //unless it is divisible by 100. However, if it is divisible by 400, then it is.
 
-const leapYear = () => {
-    
+const leapYear = (year) => {
+    if((year % 4 === 0 && year % 100 != 0) || (year % 400 === 0)) {
+        console.log('That is a leap year');
+    } else {
+        console.log('wamp wah')
+    }
 }
+
+// leapYear()
 
