9f7cc498a9d10b1076caaa78efdf6fef5366875a
Jack Piro <jackpiro1133@gmail.com>
2024-04-30 12:21:28 -0700

    update

--- Algorithms-Book/Chapter-1.js
+++ Algorithms-Book/Chapter-1.js
@@ -17,3 +17,39 @@ const onlyTheLastFew = (arr, num) => {
 onlyTheLastFew(array, 3)
 
 
+/* Soaring IQ
+Your time at the Dojo will definitely make you smarter! Let’s say a new Dojo student, Bogdan, entered
+with a modest IQ of 101. Let’s say that during a 14-week bootcamp, his IQ rose by .01 on the first day,
+then went up by an additional .02 on the second day, then up by .03 more on the third day, etc. all the
+way until increasing by .98 on his 98th day (the end of 14 full weeks). What is Bogdan’s final IQ?
+*/
+
+const bogdan = (IQ, days, incr) => {
+    modIncr = incr * 100
+    IQ = IQ * 100
+    for (let i = 0; i < days; i++) {
+        IQ = IQ + modIncr
+        modIncr = modIncr + (incr * 100)
+        console.log(incr)
+        console.log(IQ)
+    }
+    IQ = IQ % 100
+    console.log(IQ)
+}
+
+// bogdan(101, 98, 0.01)
+
+const bogdan2 = (iq, days, increment) => {
+    scaledIq = iq * 100;
+    scaledIncrement = increment * 100;
+
+    for (let i = 0; i < days; i++) {
+        scaledIq += scaledIncrement
+        scaledIncrement += (increment * 100)
+    }
+    finalIQ = scaledIq % 100;
+    console.log(finalIQ)
+}
+
+bogdan2(101, 98, 0.01)
+
